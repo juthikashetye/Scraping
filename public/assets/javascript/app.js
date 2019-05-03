da7121865c42d9297ecd99e36aa0ebc21631656d
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-05-03 14:06:36 -0700

    added save articles

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,102 +1,158 @@
 var inputVal;
-      
-      function getNews(){
-      	$('.mainDiv').empty();
-      
-      	$.ajax({
-      		method: 'GET',
-      		url: '/news'
-      	}).then(function(news){
+var currDiv;
+var currDivId;
+var favArticle;
 
-      		for (var nIndex in news){
-      
-      			var newsDiv = $("<div>");
-      			newsDiv.html(`Title: ${news[nIndex].title} <br>Link: ${news[nIndex].link} <br>Story: ${news[nIndex].story} <br>`);
+function getNews() {
+  $('#mainDiv').empty();
 
-            // var saveButton = $("<button>").attr("class", "save").text("Save Article");
-      
-      			var commentDiv = $("<div>").attr("class", "commentDiv")
-      									  .attr("id","commentDiv"+news[nIndex]._id);
-      
-      			var input = $('<input>').attr("class", "comment")
-      									.attr("id", "input"+news[nIndex]._id);
+  $.ajax({
+    method: 'GET',
+    url: '/news'
+  }).then(function(news) {
 
-      			var addButton = $("<button>").attr('class', 'add-comment').attr('data-id', news[nIndex]._id).text('Add comment');
-      			// getComments(commentDiv);
-            // newsDiv.append(saveButton);
-      			newsDiv.append(commentDiv);
-      			newsDiv.append(input);
-      			newsDiv.append(addButton);
-      			
-      			$('#mainDiv').append(newsDiv);
-      		}
-      	})
-      }
-      
-      getNews();
-      // getComments();
+    for (var nIndex in news) {
 
-   //    function getComments(cd){
+      var newsDiv = $("<div>");
+      newsDiv.html(`<span class="titleSpan">Title: ${news[nIndex].title}</span> <br> 
+                     <span class="storySpan">Story: ${news[nIndex].story}</span> <br>
+                     <a class="storyLink" href="${news[nIndex].link}">See full story</a> <br>`);
 
-   //    	$.ajax({
-   //    		method: 'GET',
-   //    		url: '/all-comments'
-   //    	}).then(function(c){
+      var saveButton = $("<button>").attr("class", "saveArticle").text("Save Article");
 
-   //    		for (var cIndex in c){
-   //    		var prevComments = $("<div>").attr("class", "prevCommentsDiv").html(c[cIndex].comment);
-   //    		cd.append(prevComments);
-			// }
-   //    	})
-   //    }
-      
-      $(document).on('click', '.add-comment', function(e){
-      
-      	e.preventDefault();
-      
-      	inputVal = $(this).prev().val();
-      	var currDiv = $(this).prev().prev(".commentDiv");
+      newsDiv.append(saveButton);
 
-      	console.log(inputVal);
+      $('#mainDiv').append(newsDiv);
+    }
+  })
+}
 
-      	$.ajax({
-      		method: 'POST',
-      		url: `/addComment`,
-      		data: {
-      			comment: inputVal
-      		}
-      	}).then(function(cid){
-      
-      		console.log(cid);
-      		console.log(cid.comment);
-      		console.log(cid._id);
+// getComments(currDivId);
 
-      		var commentPara = $("<p>").text(cid.comment).attr("id", "commentPara"+cid._id);
+function getComments() {
 
-      		var deleteButton = $('<button>').attr('class', 'delete-comment').attr('data-id', cid._id).text('Delete comment');
-      
-      		commentPara.append(deleteButton);
-      		currDiv.append(commentPara);
-      
-      		// getNews();
-      		// getComments();
-      	})
-      })
-      
-      
-      $(document).on('click', '.delete-comment', function(){
+  $.ajax({
+    method: 'GET',
+    url: "/all-comments"
+  }).then(function(c) {
 
-        var $button = $(this);
+    for (var cIndex in c) {
+      var prevComments = $("<div>").attr("class", "prevCommentsDiv").html(c[cIndex].comment);
+      // currDiv.append(prevComments);
+    }
+  })
+}
 
-      	var id = $(this).attr('data-id');
-      
-      	$.ajax({
-      		method: 'DELETE',
-      		url: `/comment/${id}`
-      	}).then(function(idd){
-      		console.log(idd);
-         $button.parent().remove();
-      		// getNews();
-      		// getComments();
-      	})
-      })
+function getFavNews() {
+  $('#favArticlesDiv').empty();
+
+  $.ajax({
+    method: 'GET',
+    url: '/savedArticles'
+  }).then(function(sNews) {
+
+    for (var sIndex in sNews) {
+
+      var sNewsDiv = $("<div>");
+      sNewsDiv.html(`<span class="titleSpan">Title: ${sNews[sIndex].title}</span> <br> 
+                     <span class="storySpan">Story: ${sNews[sIndex].story}</span> <br>
+                     <a class="storyLink" href="${sNews[sIndex].link}">See full story</a> <br>`);
+
+      var commentDiv = $("<div>").attr("class", "commentDiv")
+        .attr("id", "commentDiv" + sNews[sIndex]._id);
+
+      var input = $('<input>').attr("class", "comment")
+        .attr("id", "input" + sNews[sIndex]._id);
+
+      var addButton = $("<button>").attr('class', 'add-comment').attr('data-id', sNews[sIndex]._id).text('Add comment');
+
+      sNewsDiv.append(commentDiv);
+      sNewsDiv.append(input);
+      sNewsDiv.append(addButton);
+
+      $('#favArticlesDiv').append(sNewsDiv);
+    }
+  })
+}
+
+$(document).on('click', '.add-comment', function(e) {
+
+  e.preventDefault();
+
+  inputVal = $(this).prev().val();
+  currDiv = $(this).prev().prev(".commentDiv");
+  currDivId = $(this).prev().prev(".commentDiv").attr("id");
+  console.log("Comment: ", inputVal);
+
+  $.ajax({
+    method: 'POST',
+    url: `/addComment`,
+    data: {
+      comment: inputVal
+    }
+  }).then(function(cid) {
+
+    console.log("Added comment: ", cid);
+
+    var commentPara = $("<p>").text(cid.comment).attr("id", "commentPara" + cid._id);
+
+    var deleteButton = $('<button>').attr('class', 'delete-comment').attr('data-id', cid._id).text('Delete comment');
+
+    commentPara.append(deleteButton);
+    currDiv.append(commentPara);
+
+    // getNews();
+    // getComments(currDivId);
+  })
+})
+
+$(document).on('click', '.saveArticle', function(e) {
+
+  e.preventDefault();
+
+  var favArticleTitle = $(this).parent().children(".titleSpan").text();
+  var favArticleLink = $(this).parent().children(".storyLink").text();
+  var favArticleStory = $(this).parent().children(".storySpan").text();
+  $.ajax({
+    method: 'POST',
+    url: '/saved',
+    data: {
+      link: favArticleLink,
+      title: favArticleTitle,
+      story: favArticleStory
+    }
+    
+  }).then(function(s) {
+
+    console.log("Saved Article: ", s);
+    // console.log(s.comment);
+    // console.log(s._id);
+
+    // var commentPara = $("<p>").text(s.comment).attr("id", "commentPara" + s._id);
+
+    // var deleteButton = $('<button>').attr('class', 'delete-comment').attr('data-id', s._id).text('Delete comment');
+
+    // commentPara.append(deleteButton);
+    // currDiv.append(commentPara);
+
+    // getNews();
+    // getComments();
+  })
+})
+
+$(document).on('click', '.delete-comment', function() {
+
+  var delCommentButton = $(this);
+
+  var id = $(this).attr('data-id');
+
+  $.ajax({
+    method: 'DELETE',
+    url: `/comment/${id}`
+  }).then(function(idd) {
+    console.log("Deleted comment id: ", idd);
+    delCommentButton.parent().remove();
+    // getNews();
+    // getComments();
+  })
+})
--- server.js
+++ server.js
@@ -33,13 +33,77 @@ app.get("/news", function(req, res) {
 
   // Find all results from the scrapedNews collection in the db
   db.scrapedNews.find({}, function(error, found) {
-    // Throw any errors to the console
+
     if (error) {
       console.log(error);
     }
-    // If there are no errors, send the data to the browser as json
     else {
       res.json(found);
+    }
+  });
+});
+
+//Handle save button onclick event, save article to mongo 
+app.post("/saved", function(req, res) {
+
+  //insert into savedArticles collection
+  db.savedArticles.insert({
+    link: req.body.link,
+    title: req.body.title,
+    story: req.body.story
+  }, function(error, saved) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(saved);
+    }
+  });
+});
+
+// Retrieve all saved articles from the db
+app.get("/savedArticles", function(req, res) {
+
+  // Find all results from the savedArticles collection in the db
+  db.savedArticles.find({}, function(error, found) {
+
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Handle form submission, save submission to mongo
+app.post("/addComment", function(req, res) {
+  
+  // Insert the comment into the comments collection
+  db.comments.insert({
+    comment: req.body.comment
+  }, function(error, savedComment) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    } else {
+      //the reason why we are sending the savedComment back is because 
+      //we now have an _id to give to the client
+      res.json(savedComment);
+    }
+  });
+});
+
+app.delete("/comment/:id", function(req, res) {
+
+  var id = req.params.id;
+
+  db.comments.remove({
+    "_id": mongojs.ObjectID(id)
+  }, function(error, removed) {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(id);
     }
   });
 });
@@ -49,21 +113,86 @@ app.get("/all-comments", function(req, res) {
 
   // Find all results from the scrapedNews collection in the db
   db.comments.find({}, function(error, found) {
-    // Throw any errors to the console
+
     if (error) {
       console.log(error);
     }
-    // If there are no errors, send the data to the browser as json
     else {
       res.json(found);
     }
   });
 });
 
+// Scrape data from the onion site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
 
-app.delete("/comment/:id", function(req, res) {
+  // Make a request via axios for the news section of `theonion`
+  axios.get("https://www.theonion.com/c/news-in-brief").then(function(response) {
+    // Load the html body from axios into cheerio
+    var $ = cheerio.load(response.data);
 
-  // TRY THIS
+    // fs.writeFileSync('stuff.txt', response.data);
+
+    $(".item__text").each(function(i, element) {
+
+      var title = $(element).children("h1").children("a").children("div").text();
+      var link = $(element).children("h1").children("a").attr("href");
+      var story = $(element).children(".entry-summary").children("p").text();
+
+      console.log(title);
+      console.log(link);
+      console.log(story);
+
+        // If this found element had all title, link and story
+        if (link && title && story) {
+          // Insert the data in the scrapedNews db
+          db.scrapedNews.insert({
+              link: link,
+              title: title,
+              story: story
+            },
+            function(err, inserted) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(inserted);
+              }
+            });
+        }
+    });
+  });
+
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
+
+// TRY THIS
+ // app.post("/addComment/:id", function(req, res) { 
+ //  db.scrapedNews.update(
+ //    {_id: mongojs.ObjectID(id)},  
+ //      { $push: 
+ //        { comments: 
+ //          {
+ //            _id: mongojs.ObjectId(),
+ //            comment: comment
+ //          }
+ //        }
+ //      },
+ //      function(error, savedComment){
+ //        if (error) {
+ //          console.log(error)
+ //        } else {
+ //          res.json(savedComment);
+ //        }
+ //    });
+ // });
+
+ // TRY THIS
 // app.delete("/comment/:articleId/:commentId", function(req, res) {
 //   db.scrapedNews.update(
 //       {_id: mongojs.ObjectId(articleId)},  
@@ -80,106 +209,4 @@ app.delete("/comment/:id", function(req, res) {
 //         }
 //         res.json(commentId);
 //       });
-
-  var id = req.params.id;
-
-  db.comments.remove({
-    "_id": mongojs.ObjectID(id)
-  }, function(error, removed) {
-    if (error) {
-      res.send(error);
-    } else {
-      res.json(id);
-    }
-  });
-});
-
-// Handle form submission, save submission to mongo
-app.post("/addComment", function(req, res) {
-
-  // TRY THIS
- // app.post("/addComment/:id", function(req, res) { 
-  // db.scrapedNews.update(
-  //     {_id: mongojs.ObjectID(id)},  
-  //     { $push: 
-  //       { comments: 
-  //         {
-  //           _id: mongojs.ObjectId(),
-  //           comment: comment
-  //         }
-  //       }
-  //     },
-  //     function(error, savedComment){
-  //       if (error) {
-  //         console.log(error)
-  //       } else {
-  //         res.json(savedComment);
-  //       }
-  //     });
-    
-  // Insert the comment into the comments collection
-  db.comments.insert({
-    comment: req.body.comment
-  }, function(error, savedComment) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    } else {
-      //the reason why we are sending the savedComment back is because 
-      //we now have an _id to give to the client
-      res.json(savedComment);
-    }
-  });
-});
-
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-
-  // Make a request via axios for the news section of `theonion`
-  axios.get("https://www.theonion.com/c/news-in-brief").then(function(response) {
-    // Load the html body from axios into cheerio
-    var $ = cheerio.load(response.data);
-
-    // fs.writeFileSync('stuff.txt', response.data);
-
-    // For each element with a ".item__text" class
-
-    $(".item__text").each(function(i, element) {
-
-      var title = $(element).children("h1").children("a").children("div").text();
-      var link = $(element).children("h1").children("a").attr("href");
-      var story = $(element).children(".entry-summary").children("p").text();
-
-      console.log(title);
-      console.log(link);
-      console.log(story);
-
-        // If this found element had both a title and a link
-        if (link && title && story) {
-          // Insert the data in the scrapedNews db
-          db.scrapedNews.insert({
-              link: link,
-              title: title,
-              story: story
-            },
-            function(err, inserted) {
-              if (err) {
-                // Log the error if one is encountered during the query
-                console.log(err);
-              } else {
-                // Otherwise, log the inserted data
-                console.log(inserted);
-              }
-            });
-        }
-    });
-  });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
-
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
+// });
